4b404b89effa427adf8036799d21b3e9ab559d0c
jessica.gloor <jessicagloor@hotmail.com>
2024-05-14 19:48:52 -0300

    adiciona teste na camada da model

--- src/test/java/br/com/pessoas/modules/endereco/model/EnderecoTest.java
+++ src/test/java/br/com/pessoas/modules/endereco/model/EnderecoTest.java
@@ -1,8 +1,11 @@
 package br.com.pessoas.modules.endereco.model;
 
+import br.com.pessoas.modules.endereco.dto.EnderecoRequest;
+import br.com.pessoas.modules.endereco.enums.ESituacao;
 import org.junit.jupiter.api.Test;
 
 import static br.com.pessoas.modules.endereco.enums.ESituacao.PRIMARIA;
+import static br.com.pessoas.modules.endereco.helper.EnderecoHelper.umEndereco;
 import static br.com.pessoas.modules.endereco.helper.EnderecoHelper.umEnderecoRequest;
 import static br.com.pessoas.modules.pessoa.helper.PessoaHelper.umaPessoa;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,5 +18,21 @@ public class EnderecoTest {
                 .extracting("logradouro", "cep", "numero", "cidade", "uf", "situacao",
                 "pessoa.id")
                 .containsExactly(    "Rua das Flores", "86909-909", 123, "Londrina", "PR", PRIMARIA, 1);
+    }
+
+    @Test
+    public void setAll_deveAtualizarEndereco_quandoSolicitado() {
+        var endereco = umEndereco(1);
+        assertThat(endereco)
+                .extracting("logradouro", "cep", "numero", "cidade", "uf", "situacao", "pessoa.id")
+                .containsExactly("Rua das Flores", "86909-909", 123, "Londrina", "PR", PRIMARIA, 1);
+
+        var request = umEnderecoRequest();
+        request.setLogradouro("Rua Guargulio");
+        endereco.setAll(request);
+
+        assertThat(endereco)
+                .extracting("logradouro", "cep", "numero", "cidade", "uf", "situacao", "pessoa.id")
+                .containsExactly("Rua Guargulio", "86909-909", 123, "Londrina", "PR", PRIMARIA, 1);
     }
 }
