31281026fcbe125aef213a21ce6ede3e02a69a51
jessica.gloor <jessicagloor@hotmail.com>
2024-05-14 21:04:30 -0300

    adiciona anotation na camada da repository

--- src/main/java/br/com/pessoas/modules/pessoa/repository/PessoaRepository.java
+++ src/main/java/br/com/pessoas/modules/pessoa/repository/PessoaRepository.java
@@ -2,9 +2,9 @@ package br.com.pessoas.modules.pessoa.repository;
 
 import br.com.pessoas.modules.pessoa.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-    Optional<Pessoa> findById(Integer id);
+
 }
