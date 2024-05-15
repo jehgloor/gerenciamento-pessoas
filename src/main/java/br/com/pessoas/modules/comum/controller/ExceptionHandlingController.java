93de2393f79f64c4b4978794cbd222c3cdf13459
jessica.gloor <jessicagloor@hotmail.com>
2024-05-14 21:30:09 -0300

    arruma classe de excecoes para retornar os erros

--- src/main/java/br/com/pessoas/modules/comum/controller/ExceptionHandlingController.java
+++ src/main/java/br/com/pessoas/modules/comum/controller/ExceptionHandlingController.java
@@ -1,15 +1,20 @@
 package br.com.pessoas.modules.comum.controller;
 
 import br.com.pessoas.modules.comum.exception.NotFoundException;
+import br.com.pessoas.modules.comum.exception.ValidacaoException;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHandlingController {
@@ -21,11 +26,37 @@ public class ExceptionHandlingController {
         return Collections.singletonList(new Message(ex.getMessage()));
     }
 
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ValidacaoException.class)
+    public List<Message> validacaoError(ValidacaoException ex) {
+        return Arrays.asList(new Message(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public List<Message> argumentValidationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+
+        return result.getFieldErrors()
+                .stream()
+                .map(e -> e.getDefaultMessage().toLowerCase().contains("campo")
+                        ? new Message(e.getDefaultMessage())
+                        : new Message(e.getField(), "O campo " + e.getField() + " " + e.getDefaultMessage())
+                ).collect(Collectors.toList());
+    }
+
     @Data
     private static class Message {
         private String message;
+        private String field;
 
         Message(String message) {
+            this.message = message;
+        }
+
+        Message(String field, String message) {
+            this.field = field;
             this.message = message;
         }
     }
---
+++ src/main/java/br/com/pessoas/modules/comum/exception/ValidacaoException.java
@@ -0,0 +1,8 @@
+package br.com.pessoas.modules.comum.exception;
+
+public class ValidacaoException extends RuntimeException {
+
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
